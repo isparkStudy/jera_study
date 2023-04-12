ba630c3c97705aba4667e5a961b906121ab63e97
isparkStudy <130323797+isparkStudy@users.noreply.github.com>
2023-04-12 11:56:19 +0900

    C#_Debug
    부록 A. Visual Studio 디버거 사용하기(중단점, 한 단계씩 코드 실행)

---
+++ C#/Debug/DebugDemo/DebugDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+// 중단점 설정: F9
+// 디버깅: F5
+// 디버깅하지 않고 시작: CTRL+F5
+// 단계별 실행: F11
+
+class DebugDemo
+{
+    static void Main()
+    {
+        int number1 = 10;
+        int number2 = 20;
+        int number3 = 30;
+
+        Console.WriteLine(number1);
+        Console.WriteLine(number2);
+        Console.WriteLine(number3);
+    }
+}
