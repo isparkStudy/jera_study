5bbb7c7394b6a9df0718593a0e8ba415d38b22fd
isparkStudy <130323797+isparkStudy@users.noreply.github.com>
2023-04-24 11:57:12 +0900

    C#_ch62
    62. 모던 C#

---
+++ C#/ch62/PatternMachingIf/PatternMachingIf/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace PatternMachingIf
+{
+    class Shape { }
+
+    class Rectangle : Shape
+    {
+        public string Name { get; set; } = "사각형";
+    }
+
+    class PatternMachingIf
+    {
+        static void Main(string[] args)
+        {
+            ShowShape(new Rectangle());
+
+            static void ShowShape(Shape shape)
+            {
+                if (shape is Rectangle r)
+                {
+                    Console.WriteLine(r.Name);
+                }
+            }
+        }
+    }
+}
---
+++ C#/ch62/PatternMatchingWithSwitch/PatternMatchingWithSwitch/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Circle
+{
+    public int Radius { get; set; }
+}
+
+class Rectangle
+{
+    public int Length { get; set; }
+    public int Height { get; set; }
+}
+
+class PatternMatchingWithSwitch
+{
+    static void Main(string[] args)
+    {
+        PrintShape(new Circle { Radius = 10});
+        PrintShape(new Rectangle { Length = 20, Height = 10 });
+        PrintShape(new Rectangle { Length = 10, Height = 10 });
+    }
+
+    static void PrintShape(object shape)
+    {
+        switch (shape)
+        {
+            case Rectangle s when (s.Length == s.Height):
+                Console.WriteLine($"정사각형: {s.Length} x {s.Height}");
+                break;
+            case Rectangle r:
+                Console.WriteLine($"정사각형: {r.Length} x {r.Height}");
+                break;
+            case Circle c:
+                Console.WriteLine($"원: 반지름({c.Radius})");
+                break;
+            case null:
+                throw new ArgumentNullException(nameof(shape));
+            default:
+                Console.WriteLine("<기타도형>");
+                break;
+        }
+    }
+}
+
