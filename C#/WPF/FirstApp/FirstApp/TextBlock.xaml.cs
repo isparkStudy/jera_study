857f8240f1a4d3fa5e236122375e86ac599be1bc
isparkStudy <130323797+isparkStudy@users.noreply.github.com>
2023-04-24 16:58:08 +0900

    C#_WPF강좌
    C# WPF 강의 #1 프로젝트만들기
    C# WPF 강좌 #2 스택패널 stackpanel
    C# WPF 강좌 #3 그리드 Grid
    C# WPF 강좌 #4 WrapPanel 랩패널과 StackPanel 스택패널차이
    C# WPF 강좌 #5 레이블 Label 바인딩 textbox accessText
    C# WPF 강좌 #6 버튼 Button
    C# WPF 강좌 #7 텍스트블럭, 하이퍼링크
    C# WPF 강좌 #8 텍스트박스, 패스워드 박스
    C# WPF 강좌 #9 체크박스 checkbox
    C# WPF 강좌 #10 라디오 버튼, Radio Button
    C# WPF 강좌 #11 슬라이더 Slider 바인딩
    C# WPF 강좌 #12 리스트박스 Listbox 바인딩

---
+++ C#/WPF/FirstApp/FirstApp/TextBlock.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace FirstApp
+{
+    /// <summary>
+    /// ch7.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class ch7 : Window
+    {
+        public ch7()
+        {
+            InitializeComponent();
+        }
+
+        private void hyper_MouseEnter(object sender, MouseEventArgs e)
+        {
+            hyper.TextDecorations = TextDecorations.Underline;
+        }
+
+        private void hyper_MouseLeave(object sender, MouseEventArgs e)
+        {
+            hyper.TextDecorations = null;
+        }
+
+        private void hyper_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
+        {
+            System.Diagnostics.Process.Start(e.Uri.AbsoluteUri);
+        }
+
+    }
+}
