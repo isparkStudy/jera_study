324b47ccafafafa91c94670445ad42576783f131
isparkStudy <130323797+isparkStudy@users.noreply.github.com>
2023-04-27 09:50:19 +0900

    C#_WPF(6~10)
    6. WPF 트리거(Trigger)란?_프로퍼티트리거
    
    7. WPF Trigger 데이터 트리거(Data Trigger)
    
    8. ListBox와 LINQ쿼리데이터바인딩, 새창띄우기, 이벤트 및 델리게이트를 통한 메인 윈도우리스트박스리프레쉬
    
    9. 의존프로퍼티(DependencyProperty), 의존속성
    
    10. WPF이벤트라우팅(RoutedEvent), 버블링(Bubbling), 터널링(Tunneling) 이벤트

---
+++ C#/WPF/ListBoxAndLinqBindig/WpfApp1/WpfApp1/Duty.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public enum DutyType
+    {
+        Inner,
+        OutSide
+    }
+
+    public class Duty
+    {
+        private string _name;       // 직무명
+        private DutyType _dutyType;      // 직무타입(내근, 외근)
+
+        public Duty() { }
+        public Duty(string name, DutyType dutyType)
+        {
+            _name = name;
+            _dutyType = dutyType;
+        }
+
+        public string DutyName
+        { 
+            get { return _name; }
+            set
+            {
+                _name = value;
+            }
+        }
+
+        public DutyType DutyType
+        {
+            get { return _dutyType; }
+            set
+            {
+                _dutyType = value;
+            }
+        }
+    }
+
+    public class Duties : ObservableCollection<Duty>
+    {
+        public Duties()
+        {
+            Add(new Duty("SALES", DutyType.OutSide));
+            Add(new Duty("LOGISTICS", DutyType.OutSide));
+            Add(new Duty("IT", DutyType.Inner));
+            Add(new Duty("MARKETING", DutyType.Inner));
+            Add(new Duty("HR", DutyType.Inner));
+            Add(new Duty("PROPOTION", DutyType.OutSide));
+        }
+    }
+}
---
+++ C#/WPF/ListBoxAndLinqBindig/WpfApp1/WpfApp1/SubWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+
+    public partial class SubWindow : Window
+    {
+        // 메인 윈도우의 하단 ListBox를 Refresh하기 위한 델리게이트
+        // 메인 윈도우에서 직무추가 버튼을 클릭할 때 이벤트를 할당해 준다.
+        public Delegate UpdateActor;
+
+        public SubWindow()
+        {
+            InitializeComponent();
+        }
+
+        // 저장 버튼 클릭
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (rdolnner.IsChecked == false && rdoOutSide.IsChecked == false)
+            {
+                MessageBox.Show("내근 또는 외근을 선택하세요.", "항목선택");
+                return;
+            }
+
+            DutyType dutyType = (rdolnner.IsChecked == true) ? DutyType.Inner : DutyType.OutSide;
+            MainWindow.duties.Add(new Duty(txtDutyName.Text, dutyType));        // 사용자가 지금 입력한 직무
+
+            UpdateActor.DynamicInvoke(dutyType);
+
+            MessageBox.Show("저장OK!", "저장확인");
+            this.Close();
+
+        }
+    }
+}
